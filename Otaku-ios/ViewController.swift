b8238f95cef8b82668f65417209810f454488ac2
etudiant <etudiant@MacBook-Air-01.local>
2020-01-21 14:33:45 +0100

    listFilmGibli layout, viewController, affichage

--- Otaku-ios/ViewController.swift
+++ Otaku-ios/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableFilms: UITableView!
+    
+    var listOfFilms: [FilmDataResponse] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableFilms.dataSource = self
+        getFilms()
+    }
+    
+    func getFilms(){
+    AF.request("https://ghibliapi.herokuapp.com/films",method: .get) .responseDecodable { [weak self](response: DataResponse<[FilmDataResponse], AFError>) in
+        switch response.result
+        {
+        case .success(let films):
+            self?.listOfFilms = films
+            self?.tableFilms.reloadData()
+            
+            //
+        case .failure(let error):
+            print(error.errorDescription ?? "")
+        }
+    }
     }
 
+}
 
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfFilms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        if let cellDynamique = tableView.dequeueReusableCell(withIdentifier: "FilmCellID", for: indexPath) as? ListFilmTableViewCell {
+            
+            let correctFilm = listOfFilms[indexPath.row]
+            cellDynamique.fill(withFilmDataResponse: correctFilm)
+            
+            return cellDynamique
+        }else {
+            return UITableViewCell()
+        }
+    }
+    
+    
 }
 
